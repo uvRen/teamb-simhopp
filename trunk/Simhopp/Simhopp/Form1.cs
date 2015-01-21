ddbf7c2a08c3e98c98a1f389cf315154fc11e70c
teamb@tuffast.com <teamb@tuffast.com@37d6f8be-a54e-80fe-98f9-43b76b336a2a>
2015-01-21 09:48:37 +0000

    Added MySql.Data.dll

--- trunk/Simhopp/Simhopp/Form1.cs
+++ trunk/Simhopp/Simhopp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Simhopp
 {
@@ -15,6 +16,25 @@ namespace Simhopp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void dbtest()
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=tuffast.com;uid=teamb;pwd=teambteamb;database=db_teamb;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
