df81edc8d4ce682487d62f9f18c8bb26af1eb28c
teamb@tuffast.com <teamb@tuffast.com@37d6f8be-a54e-80fe-98f9-43b76b336a2a>
2015-01-26 11:13:39 +0000

    Simon

--- trunk/Simhopp/Simhopp/Judge.cs
+++ trunk/Simhopp/Simhopp/Judge.cs
@@ -25,8 +25,49 @@ namespace Simhopp
             this.name = name;
         }
 
+        //ansluter till databasen och returnerar anslutningen
+        private MySql.Data.MySqlClient.MySqlConnection connectToDatabase()
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString = "server=tuffast.com;uid=teamb;pwd=teambteamb;database=db_teamb;";
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+                return conn;
+            }
+
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
+
         //medlemsfunktioner
         public void addJudgeToDatabase()
+        {
+            //ansluter till databasen
+            MySql.Data.MySqlClient.MySqlConnection conn = connectToDatabase();
+
+            //om anslutningen misslyckades
+            if (conn != null)
+            {
+                //lägger till domaren i databasen
+                MySqlCommand comm = conn.CreateCommand();
+                comm.CommandText = "INSERT INTO judge(id,name) VALUES(@id, @name)";
+                comm.Parameters.AddWithValue("@id", this.ID);
+                comm.Parameters.AddWithValue("@name", this.name);
+                comm.ExecuteNonQuery();
+                conn.Close();
+            }
+            
+           
+        }
+
+        public void readJudgeFromDatabase(int ID)
         {
             //ansluter till databasen
             MySql.Data.MySqlClient.MySqlConnection conn;
@@ -50,7 +91,6 @@ namespace Simhopp
             {
                 Console.WriteLine(ex.Message);
             }
-            
         }
         public void setJudgeName(string name)
         {
