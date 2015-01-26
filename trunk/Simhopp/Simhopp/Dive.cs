c2347e126ea3e776016a011231e0397e8f537192
teamb@tuffast.com <teamb@tuffast.com@37d6f8be-a54e-80fe-98f9-43b76b336a2a>
2015-01-26 10:03:28 +0000

    Simon

--- trunk/Simhopp/Simhopp/Dive.cs
+++ trunk/Simhopp/Simhopp/Dive.cs
@@ -8,6 +8,28 @@ namespace Simhopp
 {
     public class Dive
     {
+        private int ID;
+        private Jumper person;
+        private double difficulty;
+        private Competition comp;
 
+        //konstruktor
+        public Dive()
+        {
+            this.ID = -1;
+            this.person = null;
+            this.difficulty = 0.0;
+            this.comp = null;
+        }
+
+        public Dive(int ID, Jumper person, double difficulty, Competition comp)
+        {
+            this.ID = ID;
+            this.person = person;
+            this.difficulty = difficulty;
+            this.comp = comp;
+        }
+
+        //medlemsfunktioner
     }
 }
--- trunk/Simhopp/Simhopp/Score.cs
+++ trunk/Simhopp/Simhopp/Score.cs
@@ -8,6 +8,28 @@ namespace Simhopp
 {
     public class Score
     {
+        private int ID;
+        private Dive d;
+        private Judge j;
+        private double poang;
 
+        //konstruktor
+        public Score()
+        {
+            this.ID = -1;
+            this.d = null;
+            this.j = null;
+            this.poang = 0.0;
+        }
+
+        public Score(int ID, Dive d, Judge j, double poang)
+        {
+            this.ID = ID;
+            this.d = d;
+            this.j = j;
+            this.poang = poang;
+        }
+
+        //medlemsfunktioner
     }
 }
