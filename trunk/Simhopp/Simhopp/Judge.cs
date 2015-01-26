cdf1ed30c95b4bc3098ff49a3b2edac952d19cf0
teamb@tuffast.com <teamb@tuffast.com@37d6f8be-a54e-80fe-98f9-43b76b336a2a>
2015-01-26 11:30:48 +0000

    Simon

--- trunk/Simhopp/Simhopp/Judge.cs
+++ trunk/Simhopp/Simhopp/Judge.cs
@@ -26,7 +26,7 @@ namespace Simhopp
         }
 
         //ansluter till databasen och returnerar anslutningen
-        private MySql.Data.MySqlClient.MySqlConnection connectToDatabase()
+        public MySql.Data.MySqlClient.MySqlConnection connectToDatabase()
         {
             MySql.Data.MySqlClient.MySqlConnection conn;
             string myConnectionString = "server=tuffast.com;uid=teamb;pwd=teambteamb;database=db_teamb;";
@@ -47,12 +47,14 @@ namespace Simhopp
         }
 
         //medlemsfunktioner
-        public void addJudgeToDatabase()
+
+        //lägger till domare i databasen, returnerar TRUE om det lyckas
+        public bool addJudgeToDatabase()
         {
             //ansluter till databasen
             MySql.Data.MySqlClient.MySqlConnection conn = connectToDatabase();
 
-            //om anslutningen misslyckades
+            //om anslutningen lyckades
             if (conn != null)
             {
                 //lägger till domaren i databasen
@@ -62,36 +64,14 @@ namespace Simhopp
                 comm.Parameters.AddWithValue("@name", this.name);
                 comm.ExecuteNonQuery();
                 conn.Close();
+                return true;
             }
-            
-           
+            else
+            {
+                return false;
+            }
         }
 
-        public void readJudgeFromDatabase(int ID)
-        {
-            //ansluter till databasen
-            MySql.Data.MySqlClient.MySqlConnection conn;
-            string myConnectionString = "server=tuffast.com;uid=teamb;pwd=teambteamb;database=db_teamb;";
-            try
-            {
-                conn = new MySql.Data.MySqlClient.MySqlConnection();
-                conn.ConnectionString = myConnectionString;
-                conn.Open();
-
-                //lägger till domaren i databasen
-                MySqlCommand comm = conn.CreateCommand();
-                comm.CommandText = "INSERT INTO judge(id,name) VALUES(@id, @name)";
-                comm.Parameters.AddWithValue("@id", this.ID);
-                comm.Parameters.AddWithValue("@name", this.name);
-                comm.ExecuteNonQuery();
-                conn.Close();
-            }
-
-            catch (MySql.Data.MySqlClient.MySqlException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
         public void setJudgeName(string name)
         {
             this.name = name;
