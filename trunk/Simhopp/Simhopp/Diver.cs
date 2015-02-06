26a375b9e28676f5e06719dd88a6614752f5d90d
teamb@tuffast.com <teamb@tuffast.com@37d6f8be-a54e-80fe-98f9-43b76b336a2a>
2015-02-06 14:31:44 +0000

    attributerade dives

--- trunk/Simhopp/Simhopp/Diver.cs
+++ trunk/Simhopp/Simhopp/Diver.cs
@@ -8,7 +8,7 @@ namespace Simhopp
 {
     public class Diver
     {
-        private List<Dive> dives;
+        private List<Dive> dives {get; set;}
         public int ID { get; set; }
         public string name { get; set; }
 
