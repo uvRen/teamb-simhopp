35b86b455e213b38f4c0be133fbbf771de5f832f
teamb@tuffast.com <teamb@tuffast.com@37d6f8be-a54e-80fe-98f9-43b76b336a2a>
2015-02-09 13:42:49 +0000

    Massor

--- trunk/Simhopp/Simhopp/FormEvent.Designer.cs
+++ trunk/Simhopp/Simhopp/FormEvent.Designer.cs
@@ -31,19 +31,22 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDoDive = new System.Windows.Forms.Button();
+            this.btnNextRound = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(6, 19);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(829, 275);
+            this.tabControl1.Size = new System.Drawing.Size(409, 275);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -51,7 +54,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(821, 249);
+            this.tabPage1.Size = new System.Drawing.Size(401, 249);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -61,32 +64,43 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(501, 249);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // groupBox1
+            // btnDoDive
             // 
-            this.groupBox1.Controls.Add(this.tabControl1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(841, 300);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.btnDoDive.Location = new System.Drawing.Point(539, 257);
+            this.btnDoDive.Name = "btnDoDive";
+            this.btnDoDive.Size = new System.Drawing.Size(153, 23);
+            this.btnDoDive.TabIndex = 2;
+            this.btnDoDive.Text = "Bedöm nästa hopp";
+            this.btnDoDive.UseVisualStyleBackColor = true;
+            this.btnDoDive.Click += new System.EventHandler(this.btnDoDive_Click);
+            // 
+            // btnNextRound
+            // 
+            this.btnNextRound.Location = new System.Drawing.Point(539, 283);
+            this.btnNextRound.Name = "btnNextRound";
+            this.btnNextRound.Size = new System.Drawing.Size(153, 23);
+            this.btnNextRound.TabIndex = 3;
+            this.btnNextRound.Text = "Nästa runda";
+            this.btnNextRound.UseVisualStyleBackColor = true;
+            this.btnNextRound.Click += new System.EventHandler(this.btnNextRound_Click);
             // 
             // FormEvent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(865, 487);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(968, 576);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btnNextRound);
+            this.Controls.Add(this.btnDoDive);
             this.Name = "FormEvent";
             this.Text = "FormEvent";
             this.Load += new System.EventHandler(this.FormEvent_Load);
             this.tabControl1.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -96,6 +110,7 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDoDive;
+        private System.Windows.Forms.Button btnNextRound;
     }
 }
--- trunk/Simhopp/Simhopp/FormEvent.cs
+++ trunk/Simhopp/Simhopp/FormEvent.cs
@@ -12,66 +12,86 @@ namespace Simhopp
 {
     public partial class FormEvent : Form
     {
+        private List<List<Panel>> panels;
+        private int currentDiverIndex;
+        private int currentRoundIndex;
+        private int currentJudgeIndex;
 
-        private Event e;
+        private List<Color> colors;
+
+        private Event ev;
+        private List<Diver> divers;
         private List<Judge> judges;
-        private Diver d1, d2, d3;
         public FormEvent()
         {
             InitializeComponent();
+            colors = new List<Color>();
+
+            colors.Add(Color.FromArgb(113, 119, 128));
+            colors.Add(Color.FromArgb(76, 95, 120));
+            colors.Add(Color.FromArgb(44, 72, 112));
+            colors.Add(Color.FromArgb(15, 52, 104));
+            colors.Add(Color.FromArgb(3, 42, 93));
 
             #region testtävling
-            judges = new List<Judge>();
-            e = new Event(0, "Test", "Test", "Test", 1, 1, 5, 5);
+            
+            ev = new Event(0, "Test", "Test", "Test", 1, 1, 5, 5);
 
-            judges.Add(new Judge(0, "Mr. Test"));
-            judges.Add(new Judge(1, "Mrs. Fest"));
-            judges.Add(new Judge(2, "Konstapel Kuk"));
-            judges.Add(new Judge(3, "Domherre"));
-            judges.Add(new Judge(4, "McFlash"));
+            ev.AddJudge(new Judge(0, "Mr. Test"));
+            ev.AddJudge(new Judge(1, "Mrs. Fest"));
+            ev.AddJudge(new Judge(2, "Mr. Mega"));
+            ev.AddJudge(new Judge(3, "Mr. Domherre"));
+            ev.AddJudge(new Judge(4, "Mr. McFlash"));
+            /*
+            ev.AddJudge(new Judge(5, "Mr. Dunder"));
+            ev.AddJudge(new Judge(6, "Mr. Mega"));
+            ev.AddJudge(new Judge(7, "Mr. Bleh bleh"));
+            ev.AddJudge(new Judge(8, "Mr. Tjalala"));
+             * */
 
-            e.AddJudges(judges);
+            ev.AddDiver(new Diver(0, "Kalle"));
+            ev.AddDiver(new Diver(0, "Greger"));
+            ev.AddDiver(new Diver(0, "Hopptjej"));
+            ev.AddDiver(new Diver(0, "Annika"));
 
-            d1 = new Diver(0, "Kalle");
-            d2 = new Diver(0, "Greger");
-            d3 = new Diver(0, "Skitunge");
-
-            e.AddDiver(d1);
-            e.AddDiver(d2);
-            e.AddDiver(d3);
-
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 5; j++)
                 {
-                    Dive dive = new Dive(0, null, j + 1, e);
-                    e.GetDivers()[i].AddDive(dive);
+                    Dive dive = new Dive(0, null, j + 1, ev);
+                    ev.GetDivers()[i].AddDive(dive);
 
                     for (int k = 0; k < 5; k++)
                     {
-                        Score s = new Score(0, null, judges[k], (k + j) % 11);
-                        dive.AddScore(s);
+                        //Score s = new Score(0, null, ev.GetJudges()[k], (k + j) % 11);
+                        //dive.AddScore(s);
                     }
                 }
             }
             #endregion
 
-            tabControl1.TabPages.Clear();
-            groupBox1.Height = 55 + e.GetDivers().Count * 80;
-            tabControl1.Height = 30 + e.GetDivers().Count * 80;
+            panels = new List<List<Panel>>();
 
-            for (int i = 0; i < e.diveCount; i++)
+            currentDiverIndex = currentRoundIndex = currentJudgeIndex = 0;
+
+            tabControl1.TabPages.Clear();
+            tabControl1.Height = 30 + ev.GetDivers().Count * 80;
+
+            for (int i = 0; i < ev.diveCount; i++)
             {
-                tabControl1.TabPages.Add("Round " + (i + 1));
-                
-                for (int j = 0; j < e.GetDivers().Count(); j++)
+                panels.Add(new List<Panel>());
+                tabControl1.TabPages.Add("Runda " + (i + 1));
+                tabControl1.SelectedTab.BorderStyle = BorderStyle.None;
+                tabControl1.SelectedTab.BackColor = colors[0];
+                tabControl1.SelectedTab.ForeColor = colors[0];
+                for (int j = 0; j < ev.GetDivers().Count(); j++)
                 {
-                    Panel p = DivePanel(e.GetDivers()[j], i);
+                    Panel p = DivePanel(ev.GetDivers()[j], i);
+                    panels[i].Add(p);
                     p.Top = (p.Height + 2) * j;
                     tabControl1.TabPages[i].Controls.Add(p);
                 }
             }
-
 
         }
 
@@ -82,7 +102,9 @@ namespace Simhopp
 
         private void FormEvent_Load(object sender, EventArgs e)
         {
-
+            judges = ev.GetJudges();
+            divers = ev.GetDivers();
+            this.BackColor = colors[0];
         }
 
         Panel DivePanel(Diver diver, int round)
@@ -104,12 +126,12 @@ namespace Simhopp
                FontStyle.Bold,
                GraphicsUnit.Pixel);
 
-            int pWidth = 360;
+            int pWidth = 402;
             Panel p = new Panel();
-            p.BorderStyle = BorderStyle.Fixed3D;
+            p.BorderStyle = BorderStyle.FixedSingle;
             p.Width = pWidth;
             p.Height = 80;
-            p.BackColor = Color.FromArgb(50, 50, 50);
+            p.BackColor = colors[0];
 
             Label name = new Label();
             name.Text = diver.name;
@@ -122,13 +144,26 @@ namespace Simhopp
 
 
             Label difficulty = new Label();
-            difficulty.Text = "Difficulty: " + dive.difficulty.ToString();// "Difficulty 3"; //dive.difficulty;
+            difficulty.Text = "Svårighetsgrad: " + dive.difficulty.ToString();
             difficulty.Top = 26;
             difficulty.Left = 20;
-            difficulty.ForeColor = Color.DarkGray;
+            difficulty.Width = p.Width / 2;
+            difficulty.ForeColor = Color.White;
+            difficulty.Font = new Font(FontFamily.GenericSerif, 10, FontStyle.Bold);
 
             p.Controls.Add(difficulty);
+            
 
+            Label points = new Label();
+            points.Text = "";
+            points.Left = p.Width - points.Width - 10;
+            points.Top = 10;
+            points.Font = fontName;
+            points.TextAlign = ContentAlignment.MiddleRight;
+            points.ForeColor = Color.White;
+            points.Name = "Points";
+
+            p.Controls.Add(points);
 
             Panel scorePanel = new Panel();
             scorePanel.Width = pWidth;
@@ -136,11 +171,12 @@ namespace Simhopp
             scorePanel.Left = 0;
             scorePanel.Top = 50;
             scorePanel.BackColor = Color.Transparent;
+            scorePanel.Tag = points;
 
-            for (int i = 0; i < dive.GetScores().Count; i++)
+            for (int i = 0; i < ev.GetJudges().Count; i++)
             {
                 TextBox tb = new TextBox();
-                tb.Text = diver.dives[round].GetScores()[i].points.ToString();
+                tb.Text = " ";//diver.dives[round].GetScores()[i].points.ToString();
                 tb.Left = 25 + i * 40;
                 tb.Width = 25;
                 tb.TextAlign = HorizontalAlignment.Center;
@@ -149,6 +185,8 @@ namespace Simhopp
                 tb.Height = 25;
                 tb.Font = fontScore;
                 tb.ForeColor = Color.Black;
+                tb.Name = "Score";
+                tb.TextChanged += UpdateScore;
 
                 scorePanel.Controls.Add(tb);
             }
@@ -156,6 +194,132 @@ namespace Simhopp
             p.Controls.Add(scorePanel);
 
             return p;
+        }
+
+        void UpdateScore(object sender, EventArgs e)
+        {
+            TextBox tb = ((TextBox)sender);
+            double newScorePoint;
+            if (tb.Tag == null || !Double.TryParse(tb.Text, out newScorePoint))
+                return;
+
+            Score score = (Score)(tb.Tag);
+            score.points = newScorePoint;
+
+            Panel scorePanel = (Panel)(tb.Parent);
+            Label points = (Label)(scorePanel.Tag);
+            points.Text = score.dive.score.ToString();
+        }
+
+        private void btnDoDive_Click(object sender, EventArgs e)
+        {
+            ScoreDive();
+        }
+
+        private void btnNextRound_Click(object sender, EventArgs e)
+        {
+            btnDoDive.Enabled = true;
+            btnNextRound.Enabled = false;
+            tabControl1.SelectedIndex++;
+        }
+
+        private void ScoreDive()
+        {
+            panels[currentRoundIndex][currentDiverIndex].BackColor = colors[2];
+            Panel p = PanelScoring();
+            this.Controls.Add(p);
+            p.BringToFront();
+        }
+
+        private string ScoreTitle()
+        {
+            string str = "";
+            str += divers[currentDiverIndex].name;
+            str += ", hopp " + (currentRoundIndex + 1).ToString();
+            str += "    Domare: " + judges[currentJudgeIndex].name;
+            return str;
+        }
+
+        private Panel PanelScoring()
+        {
+            Panel p = new Panel();
+            p.BorderStyle = BorderStyle.FixedSingle;
+            p.Width = 730;
+            p.Height = 200;
+            p.BackColor = colors[1];
+
+            Label judgeName = new Label();
+            judgeName.Text = ScoreTitle();
+            judgeName.Width = p.Width;
+            judgeName.TextAlign = ContentAlignment.MiddleCenter;
+            judgeName.Font = new Font(new Font(new FontFamily("Cambria"), 16), FontStyle.Bold);
+            judgeName.ForeColor = colors[3];
+            judgeName.Top = 10;
+            judgeName.Name = "Judge";
+            p.Controls.Add(judgeName);
+
+            for (int i = 0; i < 21; i++)
+            {
+                Button btn = new Button();
+                btn.Width = 60;
+                btn.Height = 60;
+                btn.Text = (i * 0.5).ToString();
+                btn.Left = 10 + 32 * i - (i % 2) * 32 + ((i % 2))*32; 
+                btn.Top = 45 + (i % 2) * 65; //Varannan uppe / nere
+                btn.BackColor = colors[0];
+                btn.ForeColor = Color.White;
+                btn.FlatStyle = FlatStyle.Flat;
+
+                btn.Font = new Font(new Font(FontFamily.GenericSerif, 20), FontStyle.Bold);
+                btn.Tag = p;
+                btn.Click += btnScoreClick;
+                p.Controls.Add(btn);
+            }
+
+            p.Left = (this.Width / 2) - (p.Width / 2);
+            p.Top = (this.Height / 2) - (p.Height / 2);
+
+            return p;
+        }
+
+        private void btnScoreClick(object sender, EventArgs e)
+        {
+
+            Panel p = (Panel)((Button)sender).Tag;
+            
+            Score score = new Score(-1, divers[currentDiverIndex].dives[currentRoundIndex], judges[currentJudgeIndex], Double.Parse(((Button)sender).Text));
+            divers[currentDiverIndex].dives[currentRoundIndex].AddScore(score);
+            
+            panels[currentRoundIndex][currentDiverIndex].Controls.Find("Score", true)[currentJudgeIndex].Text = ((Button)sender).Text;
+            panels[currentRoundIndex][currentDiverIndex].Controls.Find("Score", true)[currentJudgeIndex].Tag = score;
+            currentJudgeIndex++;
+
+
+            if (currentJudgeIndex >= judges.Count)
+            {
+                p.Dispose();
+                currentJudgeIndex = 0;
+
+                panels[currentRoundIndex][currentDiverIndex].BackColor = colors[1];
+
+                //Visa hoppets totala poäng
+                panels[currentRoundIndex][currentDiverIndex].Controls.Find("Points", true)[0].Text = divers[currentDiverIndex].dives[currentRoundIndex].score.ToString();
+
+                //Nästa hoppare
+                currentDiverIndex++;
+                if (currentDiverIndex >= divers.Count)
+                {
+                    currentDiverIndex = 0;
+                    btnDoDive.Enabled = false;
+                    btnNextRound.Enabled = true;
+                    currentRoundIndex++;
+                }
+            }
+            else
+            {
+                //Visa nästa domare
+                p.Controls.Find("Judge", false)[0].Text = ScoreTitle();
+            }
         }
     }
 }
