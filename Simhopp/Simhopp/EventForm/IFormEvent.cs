74c644fb28ccc573dbe9a0ef72aa165e2584817a
teamb <teamb@tuffast.com>
2015-02-16 11:40:38 +0000

    sd

--- Simhopp/Simhopp/EventForm/IFormEvent.cs
+++ Simhopp/Simhopp/EventForm/IFormEvent.cs
@@ -6,16 +6,12 @@ using System.Threading.Tasks;
 
 namespace Simhopp
 {
-    interface IFormEvent
+    public interface IFormEvent
     {
         EventPresenter Presenter { get; set; }
-        public int CurrentDiverIndex { get; set; }
-        public int CurrentRoundIndex { get; set; }
-        public int CurrentJudgeIndex { get; set; }
-        public double CurrentDiveScore { get; set; }
-        public List<Diver> divers { get; set; }
+        double CurrentDiveScore { get; set; }
 
-        public void CompleteDive();
-        public void UpdateLeaderboard();
+        void CompleteDive();
+        void UpdateLeaderboard();
     }
 }
