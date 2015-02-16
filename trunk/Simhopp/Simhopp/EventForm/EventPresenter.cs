e2148f66dfcb79656c010f8945037427ee10c385
teamb@tuffast.com <teamb@tuffast.com@37d6f8be-a54e-80fe-98f9-43b76b336a2a>
2015-02-16 11:27:20 +0000

    mvpat lite, fattar nada

--- trunk/Simhopp/Simhopp/EventForm/EventPresenter.cs
+++ trunk/Simhopp/Simhopp/EventForm/EventPresenter.cs
@@ -8,5 +8,68 @@ namespace Simhopp
 {
     class EventPresenter
     {
+        
+        public IFormEvent view;
+
+        private List<Diver> divers;
+        private List<Judge> judges;
+        public Diver CurrentDiver
+        {
+            get
+            {
+                return divers[CurrentDiverIndex];
+            }
+        }
+        public Judge CurrentJudge
+        {
+            get
+            {
+                return judges[CurrentJudgeIndex];
+            }
+        }
+
+        public Dive CurrentDive
+        {
+            get
+            {
+                return CurrentDiver.dives[CurrentRoundIndex];
+            }
+        }
+        public int CurrentDiverIndex { get; set; }
+        public int CurrentRoundIndex { get; set; }
+        public int CurrentJudgeIndex { get; set; }
+
+        EventPresenter(IFormEvent view)
+        {
+            this.view = view;
+        }
+
+        public Score ScoreDive(double points)
+        {
+
+            Score score = new Score(-1, CurrentDiver.dives[CurrentRoundIndex], CurrentJudge, points);
+            CurrentDiver.dives[CurrentRoundIndex].AddScore(score); //Add score to current dive
+            CurrentJudgeIndex++;
+
+            if (CurrentJudgeIndex >= judges.Count)
+            {
+                view.CurrentDiveScore = CurrentDive.score;
+
+                CurrentJudgeIndex = 0;
+                CurrentDiverIndex++;
+
+                if (CurrentDiverIndex >= divers.Count)
+                {
+                    CurrentDiverIndex = 0;
+                    CurrentRoundIndex++;
+                }
+
+                view.CompleteDive();
+                view.UpdateLeaderboard();
+            }
+
+            return score;
+
+        }
     }
 }
